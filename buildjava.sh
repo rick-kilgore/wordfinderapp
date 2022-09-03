f2e5f0064ee0f8b1f27603af1953aadf655a5437
Rick Kilgore <rick.kilgore@gmail.com>
2022-09-02 19:23:29 -0700

    add -i option for installing to android

--- buildjava.sh
+++ buildjava.sh
@@ -6,8 +6,14 @@
 PATH="/Library/Java/JavaVirtualMachines/jdk-12.jdk/Contents/Home;$PATH"
 
 gradle -p java/wordfinder jar |& tee mk.log
+gradle -p java/app assembleRelease |& tee -a mk.log
 ./clean.sh && \
 
 if test -e mk.log; then
   cat mk.log
+fi
+
+if test "x$1" = "x-i"; then
+  adb install -r java/app/build/outputs/apk/release/app-release.apk
+  adb shell am start -n org.rkilgore.wordfinderapp/.MainActivity
 fi
