8af9e3ac089aa708eb39f1141be688cf740eea9b
Rick Kilgore <rick.kilgore@gmail.com>
2022-12-19 03:00:10 -0800

    update script for moved project folder

--- wf.sh
+++ wf.sh
@@ -2,5 +2,5 @@
 
 export PATH="/Library/Java/JavaVirtualMachines/jdk-12.jdk/Contents/Home/bin:$PATH"
 
-cd $HOME/wordfinder
+cd $HOME/local/wordfinder
 java -jar java/wordfinder/build/libs/wordfinder.jar "$@"
